eba4f17e0305ae9171eda0b80acf6ed73448b2be
Rilaptra <goodgamerizqi@gmail.com>
2024-10-20 10:51:05 +0700

    new

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -37,7 +37,7 @@ export default function AdminDashboard() {
           <AdminGreeting userInfo={userInfo} />
         </div>
         <motion.div
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-8"
           initial="hidden"
           animate="visible"
           variants={{
